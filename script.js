862d823bdd57c9b8a1b22e286d5e56ec607e1a6b
iNalsur <107191821+iNalsur@users.noreply.github.com>
2023-11-02 21:10:09 +0200

    Added script on which I was working before, script idea is to make one div dissapear and other appear and vise versa

--- script.js
+++ script.js
@@ -86,3 +86,23 @@ window.addEventListener("scroll", () => {
         contactSection.style.transition = 'opacity 1s';
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const div1 = document.querySelector(".companyShortDescription");
+    const div2 = document.querySelector(".companyLargeDescription");
+
+    let currentDiv = div1;
+    let nextDiv = div2;
+
+    currentDiv.classList.add("visible");
+    nextDiv.classList.remove("visible");   
+    
+    setInterval(function() {
+        currentDiv.classList.remove("visible");
+        nextDiv.classList.add("visible");      
+
+        const tempDiv = currentDiv;
+        currentDiv = nextDiv;
+        nextDiv = tempDiv;
+    }, 10000);
+});
